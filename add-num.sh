93b6ffaf15005230a737c078d9e70ccaa9d426c4
Kamil <baldygakamil@gmail.com>
2025-03-07 12:01:56 +0100

    fix

--- add-num.sh
+++ add-num.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Exercice 5 : Créez un script qui demande à l'utilisateur d'entrer deux nombres, puis affiche la somme de ces deux nombres
 echo "Nombre 1> "
 read num1
 echo "Nombre 2> "
