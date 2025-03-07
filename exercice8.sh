990622068504b0f43067565aee7d650407d6ed62
Kamil <baldygakamil@gmail.com>
2025-03-07 13:36:55 +0100

    add file

---
+++ exercice8.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# Exercice 8 : Créez un script qui demande le nombre de fichier a créez et le nom du fichier et les créez. Exemple d'output : 3 et monFichier => monFichier1 / monFichier2 / monFichier3
