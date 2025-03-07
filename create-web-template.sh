fa73aea7bcdc15c3a9ee90f63fff10f713a10341
Kamil <baldygakamil@gmail.com>
2025-03-07 11:30:11 +0100

    fix

--- create-web-template.sh
+++ create-web-template.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mkdir -p projet/public/{css/{footer/,header/,main/},img/,js/} && touch projet/public/css/footer/style.css && touch projet/public/css/header/style.css && touch projet/public/css/main/style.css && touch projet/public/img/img{1..2} && touch projet/public/index.html && touch projet/public/js/main.js
+mkdir -p projet/public/{css/{footer/,header/,main/},img/,js/}
+touch projet/public/css/footer/style.css
+touch projet/public/css/header/style.css
+touch projet/public/css/main/style.css
+touch projet/public/img/img{1..2}
+touch projet/public/index.html
+touch projet/public/js/main.js
