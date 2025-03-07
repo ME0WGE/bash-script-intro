21a98727404dc7449f7df9a096abc8277a2495ee
Kamil <baldygakamil@gmail.com>
2025-03-07 10:26:19 +0100

    fix

---
+++ create-web-template.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mkdir -p projet/public/{css/{footer/,header/,main/},img/,js/} && touch projet/public/css/footer/style.css && touch projet/public/css/header/style.css && touch projet/public/css/main/style.css && touch projet/public/img/img{1..2} && touch projet/public/index.html && touch projet/public/js/main.js
