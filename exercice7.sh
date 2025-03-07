1693c95b8c1aa5f1acf48fffb64b870b29878d94
Kamil <baldygakamil@gmail.com>
2025-03-07 13:35:57 +0100

    add code

--- exercice7.sh
+++ exercice7.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 # Exercice 7 : Créez un script qui demande à l'utilisateur un nom de fichier et ensuite une confirmation. Si oui, ca crée le fichier sinon un message de sortie.
+echo "Saisissez le nom du fichier à créer > "
+read fichier
+echo "Confirmez-vous la création du fichier? (Y / N)> "
+read confirmation
+option="$confirmation"
+case $option in
+    "Y")
+    touch $fichier
+    echo "Le fichier a été crée avec succèes."
+    ;;
+    "N")
+    echo "Création du fichier annulée."
+    ;;
+esac
